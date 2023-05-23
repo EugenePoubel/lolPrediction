9aa277b235c6aa2fb0d89b53050d19138ec31b24
EugenePoubel <adripons123@gmail.com>
2023-05-23 13:28:02 +0000

    Merge

--- back-end/public/Script/test_model.py
+++ back-end/public/Script/test_model.py
@@ -8,7 +8,7 @@ from pytorchModel import MyModel
 # Créez une nouvelle instance du modèle avec la même architecture
 loaded_model = MyModel(input_size=347)
 
-loaded_model.load_state_dict(torch.load("Script/model.pth"))
+loaded_model.load_state_dict(torch.load("model.pth"))
 loaded_model.eval()
 
 
@@ -16,10 +16,10 @@ def main():
     import pandas as pd
     # Vérification des arguments
     if len(sys.argv) != 4:
-        #print("Usage: python prediction.py Team1 Team2 AdvancedFeatures")
+        print("Usage: python prediction.py Team1 Team2 AdvancedFeatures")
         return
     # Charger le fichier CSV
-    df = pd.read_csv("Script/dataset_Categories.csv")
+    df = pd.read_csv("dataset_Categories.csv")
     # Récupérer les noms de colonnes
     noms_colonnes = df.columns.tolist()
     noms_colonnes.remove("Team1_win")
